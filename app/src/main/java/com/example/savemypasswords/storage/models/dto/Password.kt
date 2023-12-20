a3a5dc1e1ba5d9f09ce4212d4e08188d2d735e2d
Andrey <andrtazet@gmail.com>
2023-12-20 09:25:17 +0300

    feat: passwords

---
+++ app/src/main/java/com/example/savemypasswords/storage/models/dto/Password.kt
@@ -0,0 +1,42 @@
+package com.example.savemypasswords.storage.models.dto
+
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.Transient
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+
+object LocalDateSerializer : kotlinx.serialization.KSerializer<LocalDate> {
+    private val formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.format(formatter))
+    }
+    override fun deserialize(decoder: Decoder): LocalDate {
+        return LocalDate.parse(decoder.decodeString(), formatter)
+    }
+}
+
+@Serializable()
+data class Password(
+    val login: String,
+    val password: String,
+    val site: String,
+    val comment: String,
+    @Serializable(with = LocalDateSerializer::class)
+    val created: LocalDate,
+    @Transient
+    override var id:Int = 0,
+): Identifiable
